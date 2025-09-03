06a0397fc8eb2e8e215d0e10c4094c6a89634f30
LOVISH066 <lovishgoyal066@gmail.com>
2025-09-03 22:00:29 +0530

    Add

--- code/incdec.java
+++ code/incdec.java
@@ -3,10 +3,10 @@ class incdec
     public static void main(String[] args)
     {
         int a = 32;
-        int c = a++;
-        System.out.println(c);
-        //System.out.println(a);
+        //int c = a++;
         //System.out.println(c);
-        //System.out.println(a);
+        System.out.println(a);
+        //System.out.println(c);
+        System.out.println(a);
     }
 }
