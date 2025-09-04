98c1461d2ac1452218fcfa83477e68e03f789729
LOVISH066 <lovishgoyal066@gmail.com>
2025-09-05 01:09:59 +0530

    add

---
+++ code/switchdemo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class switchdemo {
+    public static void main(String[] args) {
+        // switch
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num = sc.nextInt();
+
+        // switch sybtax
+        switch(num)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;    
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Wrong entry");
+        }
+    }    
+}
